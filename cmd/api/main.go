a401e244c0cabb7716166bba9d283a0ccdad7b07
krishkumar84 <sharmageeta847@gmail.com>
2024-12-25 02:41:17 +0530

    testing

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -28,7 +28,7 @@ func main() {
 	router := http.NewServeMux()
 
 	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello from krish and cicd and server is running"))
+		w.Write([]byte("Hello from krish and cicd and server grace full kill is running"))
 	})
 
 	router.HandleFunc("POST /api/users",users.New())
